6addba4102aced306a3362529be5ff1e1b2b8f38
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-02-19 15:10:42 +0530

    wip

--- src/components/Admin/ChangePassword.jsx
+++ src/components/Admin/ChangePassword.jsx
@@ -35,7 +35,7 @@ function ChangePassword(props) {
               </div>
             </div>
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button onClick={add} type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Add</button>
+              <button onClick={add} type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Change Password</button>
               <button onClick={()=>{props.setModal(false)}} type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
             </div>
           </div>
